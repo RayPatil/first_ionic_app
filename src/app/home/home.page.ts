7f5208a38abbeadbabb261547a0ec97fb71d3088
raypatil <raypatil9@gmail.com>
2021-06-22 16:42:07 +0530

    first ionic app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { AuthServiceService } from '../../app/auth-service.service';
+import firebase from 'firebase/app';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +12,96 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  CountryJson = environment.CountryJson;
+  OTP = '';
+  Code: any;
+  PhoneNo: any;
+  CountryCode: any = '+91';
+  showOTPInput = false;
+  OTPmessage = 'An OTP is sent to your number. You should receive it in 15 s'
+  recaptchaVerifier: firebase.auth.RecaptchaVerifier;
+  confirmationResult: any;
+  constructor(
+    private alertController: AlertController,
+    private authService: AuthServiceService
+  ) { }
+
+
+  async ionViewDidEnter() {
+    this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
+      size: 'invisible',
+      callback: (response) => {
+
+      },
+      'expired-callback': () => {
+      }
+    });
+  }
+  ionViewDidLoad() {
+    this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
+      size: 'invisible',
+      callback: (response) => {
+
+      },
+      'expired-callback': () => {
+      }
+    });
+  }
+
+  countryCodeChange($event) {
+    this.CountryCode = $event.detail.value;
+  }
+  // Button event after the nmber is entered and button is clicked
+  signinWithPhoneNumber($event) {
+    console.log('country', this.recaptchaVerifier);
+
+    if (this.PhoneNo && this.CountryCode) {
+      this.authService.signInWithPhoneNumber(this.recaptchaVerifier, this.CountryCode + this.PhoneNo).then(
+        success => {
+          this.OtpVerification();
+        }
+      );
+    }
+  }
+  async showSuccess() {
+    const alert = await this.alertController.create({
+      header: 'Success',
+      buttons: [
+        {
+          text: 'Ok',
+          handler: (res) => {
+            alert.dismiss();
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+  async OtpVerification() {
+    const alert = await this.alertController.create({
+      header: 'Enter OTP',
+      backdropDismiss: false,
+      inputs: [
+        {
+          name: 'otp',
+          type: 'text',
+          placeholder: 'Enter your otp',
+        }
+      ],
+      buttons: [{
+        text: 'Enter',
+        handler: (res) => {
+          this.authService.enterVerificationCode(res.otp).then(
+            userData => {
+              this.showSuccess();
+              console.log(userData);
+            }
+          );
+        }
+      }
+      ]
+    });
+    await alert.present();
+  }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,118 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyCNNFbkBw_Mgy21nMiVOU8XBDB2odRoCZk',
+  authDomain: 'phone-otp-65985.firebaseapp.com',
+  projectId: 'phone-otp-65985',
+  storageBucket: 'phone-otp-65985.appspot.com',
+  messagingSenderId: '482164503288',
+  appId: '1:482164503288:web:df3f58fda6e4ab5d59410d'
+  },
+  CountryJson: [
+    {
+    name: 'Australia',
+    dial_code: '+61',
+    code: 'AU'
+    },
+    {
+    name: 'Bangladesh',
+    dial_code: '+880',
+    code: 'BD'
+    },
+    {
+    name: 'Brazil',
+    dial_code: '+55',
+    code: 'BR'
+    },
+    {
+    name: 'Canada',
+    dial_code: '+1',
+    code: 'CA'
+    },
+    {
+    name: 'China',
+    dial_code: '+86',
+    code: 'CN'
+    },
+    {
+    name: 'Egypt',
+    dial_code: '+20',
+    code: 'EG'
+    },
+    {
+    name: 'France',
+    dial_code: '+33',
+    code: 'FR'
+    },
+    {
+    name: 'Iceland',
+    dial_code: '+354',
+    code: 'IS'
+    },
+    {
+    name: 'India',
+    dial_code: '+91',
+    code: 'IN'
+    },
+    {
+    name: 'Indonesia',
+    dial_code: '+62',
+    code: 'ID'
+    },
+    {
+    name: 'Iraq',
+    dial_code: '+964',
+    code: 'IQ'
+    },
+    {
+    name: 'Ireland',
+    dial_code: '+353',
+    code: 'IE'
+    },
+    {
+    name: 'Israel',
+    dial_code: '+972',
+    code: 'IL'
+    },
+    {
+    name: 'Italy',
+    dial_code: '+39',
+    code: 'IT'
+    },
+    {
+    name: 'Japan',
+    dial_code: '+81',
+    code: 'JP'
+    },
+    {
+    name: 'Pakistan',
+    dial_cod: '+92',
+    code: 'PK'
+    },
+    {
+    name: 'United Arab Emirates',
+    dial_code: '+971',
+    code: 'AE'
+    },
+    {
+    name: 'United Kingdom',
+    dial_code: '+44',
+    code: 'GB'
+    },
+    {
+    name: 'United States',
+    dial_code: '+1',
+    code: 'US'
+    },
+    ]
+
+  };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
